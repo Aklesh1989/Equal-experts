3a04e2245982cdbfaaa93fd600278b5911ebe861
Aklesh Rathaur <akleshrathaur23@gmail.com>
2025-06-27 14:44:41 +0530

    project setup

---
+++ EqualExpertTest/EqualExpertTest/EqualExpertTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  EqualExpertTestApp.swift
+//  EqualExpertTest
+//
+//  Created by Aklesh Rathaur on 27/06/25.
+//
+
+import SwiftUI
+
+@main
+struct EqualExpertTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
